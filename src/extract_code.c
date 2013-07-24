f3671f9228b5622896acb1f56eb758fdc6a67fb0
Ondřej Bílka <neleai@seznam.cz>
2013-07-24 16:33:23 +0200

    Refactor spellcheck candidate extraction.

--- src/extract_code.c
+++ src/extract_code.c
@@ -14,15 +14,28 @@
 #define HASH_TYPE char
 #include "hash.h"
 #include "common.h"
+
+#define LANG_C 1
+#define LANG_SH 2
+#define LANG_PLAIN 3
+
+#ifndef EXTRACT_COMMENT
+  # define EXTRACT_COMMENT 0
+#endif
 int
 main (int argc, char **argv)
 {
+  int lang = LANG_C;
   test_indent_off (argv[1]);
   if (TEST_EXTENSION (argv[1], ".txt") ||
       TEST_EXTENSION (argv[1], ".htm") ||
       TEST_EXTENSION (argv[1], ".html") ||
       TEST_EXTENSION (argv[1], ".texi"))
-    return 0;
+    lang = LANG_PLAIN;
+  if (TEST_EXTENSION (argv[1], ".py"))
+    lang = LANG_SH;
+
+
   int i, j, k, len;
   int incomment = 0, incomment2 = 0, insquote = 0, indquote = 0;
   char _buffer[100000], *buffer = _buffer + 1;
@@ -30,13 +43,22 @@ main (int argc, char **argv)
   char *input = malloc (100000000), *ip = input;
   char *output = malloc (100000000), *op = output;
   _buffer[0] = ' ';
+
+
+  // Treat everything in plain files as comment.
+  if (lang == LANG_PLAIN)
+    incomment = 1;
+
   while (fgets (buffer, 100000, stdin))
     {
       for (i = 0, j = 0; buffer[i]; )
 	{
+	  if (EXTRACT_COMMENT && (incomment || incomment2) && isseparator (buffer + i - 1) && isalpha (buffer[i]))
+	    if (add_word (word (buffer + i), 1))
+	      printf ("%s\n", word (buffer + i));
 	  if (incomment)
 	    {
-	      if (!cmp (buffer + i, "*/"))
+	      if (lang == LANG_C && !cmp (buffer + i, "*/"))
 		incomment = 0;
 	    }
 	  else if (incomment2)
@@ -60,15 +82,21 @@ main (int argc, char **argv)
 	    }
 	  else
 	    {
-	      if (!isalpha (buffer[i - 1]) && isalpha (buffer[i]))
+	      if (!EXTRACT_COMMENT && !isalpha (buffer[i - 1]) && isalpha (buffer[i]))
 		if (add_word (word (buffer + i), 1))
 		  printf ("%s\n", word (buffer + i));
-
-	      if (!cmp (buffer + i, "/*"))
-		incomment = 1;
-	      if (!cmp (buffer + i, "//"))
-		incomment2 = 1;
-
+	      if (lang == LANG_C)
+		{
+		  if (!cmp (buffer + i, "/*"))
+		    incomment = 1;
+		  if (!cmp (buffer + i, "//"))
+		    incomment2 = 1;
+		}
+	      if (lang == LANG_SH)
+		{
+		  if (buffer[i] == '#')
+		    incomment2 = 1;
+		}
 	      if (buffer[i] == '\'')
 		insquote = 1;
 	      if (buffer[i] == '"')
