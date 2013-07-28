0487420dbd4b726149974ba09a97440ea79d4828
Ondřej Bílka <neleai@seznam.cz>
2013-07-28 09:13:30 +0200

    Add additional filter for learning from history.

---
+++ src/filter_dictionary.c
@@ -0,0 +1,30 @@
+/* Copyright 2013 by Ondřej Bílka <neleai@seznam.cz> */
+
+/*
+   Filter dictionary by checking if words differ by at most two characters.
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LEN 10000
+int
+main (int argc, char **argv)
+{
+  int i, j;
+  unsigned char word[2][MAX_LEN];
+  int counts[2][256];
+  while (scanf ("%s %s", word[0], word[1]) != EOF)
+    {
+      for (i = 0; i < 2; i++)
+	for (j = 0; j < 256; j++)
+	  counts[i][j] = 0;
+      for (i = 0; i < 2; i++)
+	for (j = 0; word[i][j]; j++)
+	  counts[i][word[i][j]]++;
+      int diff = 0;
+      for (i = 0; i < 256; i++)
+	diff += abs (counts[0][i] - counts[1][i]);
+      if (diff < 3)
+	printf ("%-20s %s\n", word[0], word[1]);
+    }
+}
